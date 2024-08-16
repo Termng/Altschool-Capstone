d5abcfb2d13b834cb15282a850a71fbad07369ed
Torah Felix <95339155+Termng@users.noreply.github.com>
2024-08-16 14:59:13 +0100

    .....

--- app/utils/conn.py
+++ app/utils/conn.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 
-# SQLALCHEMY_DATABASE_URL = f"{settings.database_url}"
+SQLALCHEMY_DATABASE_URL = f"{settings.database_url}"
 
 # SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Merciful16@localhost/movieDB"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Merciful16@localhost/movieDB"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
